115b6c0c335d24e5fa076e13bfce96696f9229b2
dariaoui <oussama.darioui@gmail.com>
2021-01-19 20:58:45 +0100

    first-commit

---
+++ src/main/java/org/sid/bankspringbatchtuto/BankSpringBatchTutoApplication.java
@@ -0,0 +1,13 @@
+package org.sid.bankspringbatchtuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSpringBatchTutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSpringBatchTutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sid/bankspringbatchtuto/BankSpringBatchTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.bankspringbatchtuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSpringBatchTutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
